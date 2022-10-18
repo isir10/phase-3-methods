144e01da6a8f49c04737b9edc5c80a43b97895a7
isir10 <isir.malak@moringaschool.com>
2022-10-18 15:14:09 +0300

    completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+def greet (name = "isir")
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default (name ="diana")
+    puts "Hello, #{name}!"
+    puts "Hello, programmer!"
+end
+
+def add (num1=2, num2=3)
+    return num1+num2
+end
+
+def halve (num=8)
+ if num.class !=Integer
+    return nil
+ end
+    num/2
+end
